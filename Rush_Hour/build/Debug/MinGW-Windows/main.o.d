c1e77a436d4a97431bbc7eaeb7ba4615cf3f169b
Sietsewer <sietsewer@gmail.com>
2014-03-11 11:14:38 +0100

    Added project files
    + Netbeans project
    + SFML 2.1 libraries

---
+++ Rush_Hour/build/Debug/MinGW-Windows/main.o.d
@@ -0,0 +1,108 @@
+build/Debug/MinGW-Windows/main.o: main.cpp \
+ ../SFML-2.1/include/SFML/Window.hpp ../SFML-2.1/include/SFML/System.hpp \
+ ../SFML-2.1/include/SFML/Config.hpp \
+ ../SFML-2.1/include/SFML/System/Clock.hpp \
+ ../SFML-2.1/include/SFML/System/Export.hpp \
+ ../SFML-2.1/include/SFML/System/Time.hpp \
+ ../SFML-2.1/include/SFML/System/Err.hpp \
+ ../SFML-2.1/include/SFML/System/InputStream.hpp \
+ ../SFML-2.1/include/SFML/System/Lock.hpp \
+ ../SFML-2.1/include/SFML/System/NonCopyable.hpp \
+ ../SFML-2.1/include/SFML/System/Mutex.hpp \
+ ../SFML-2.1/include/SFML/System/Sleep.hpp \
+ ../SFML-2.1/include/SFML/System/String.hpp \
+ ../SFML-2.1/include/SFML/System/Thread.hpp \
+ ../SFML-2.1/include/SFML/System/Thread.inl \
+ ../SFML-2.1/include/SFML/System/ThreadLocal.hpp \
+ ../SFML-2.1/include/SFML/System/ThreadLocalPtr.hpp \
+ ../SFML-2.1/include/SFML/System/ThreadLocalPtr.inl \
+ ../SFML-2.1/include/SFML/System/Utf.hpp \
+ ../SFML-2.1/include/SFML/System/Utf.inl \
+ ../SFML-2.1/include/SFML/System/Vector2.hpp \
+ ../SFML-2.1/include/SFML/System/Vector2.inl \
+ ../SFML-2.1/include/SFML/System/Vector3.hpp \
+ ../SFML-2.1/include/SFML/System/Vector3.inl \
+ ../SFML-2.1/include/SFML/Window/Context.hpp \
+ ../SFML-2.1/include/SFML/Window/Export.hpp \
+ ../SFML-2.1/include/SFML/Window/GlResource.hpp \
+ ../SFML-2.1/include/SFML/Window/ContextSettings.hpp \
+ ../SFML-2.1/include/SFML/Window/Event.hpp \
+ ../SFML-2.1/include/SFML/Window/Joystick.hpp \
+ ../SFML-2.1/include/SFML/Window/Keyboard.hpp \
+ ../SFML-2.1/include/SFML/Window/Mouse.hpp \
+ ../SFML-2.1/include/SFML/Window/VideoMode.hpp \
+ ../SFML-2.1/include/SFML/Window/Window.hpp \
+ ../SFML-2.1/include/SFML/Window/WindowHandle.hpp \
+ ../SFML-2.1/include/SFML/Window/WindowStyle.hpp
+
+../SFML-2.1/include/SFML/Window.hpp:
+
+../SFML-2.1/include/SFML/System.hpp:
+
+../SFML-2.1/include/SFML/Config.hpp:
+
+../SFML-2.1/include/SFML/System/Clock.hpp:
+
+../SFML-2.1/include/SFML/System/Export.hpp:
+
+../SFML-2.1/include/SFML/System/Time.hpp:
+
+../SFML-2.1/include/SFML/System/Err.hpp:
+
+../SFML-2.1/include/SFML/System/InputStream.hpp:
+
+../SFML-2.1/include/SFML/System/Lock.hpp:
+
+../SFML-2.1/include/SFML/System/NonCopyable.hpp:
+
+../SFML-2.1/include/SFML/System/Mutex.hpp:
+
+../SFML-2.1/include/SFML/System/Sleep.hpp:
+
+../SFML-2.1/include/SFML/System/String.hpp:
+
+../SFML-2.1/include/SFML/System/Thread.hpp:
+
+../SFML-2.1/include/SFML/System/Thread.inl:
+
+../SFML-2.1/include/SFML/System/ThreadLocal.hpp:
+
+../SFML-2.1/include/SFML/System/ThreadLocalPtr.hpp:
+
+../SFML-2.1/include/SFML/System/ThreadLocalPtr.inl:
+
+../SFML-2.1/include/SFML/System/Utf.hpp:
+
+../SFML-2.1/include/SFML/System/Utf.inl:
+
+../SFML-2.1/include/SFML/System/Vector2.hpp:
+
+../SFML-2.1/include/SFML/System/Vector2.inl:
+
+../SFML-2.1/include/SFML/System/Vector3.hpp:
+
+../SFML-2.1/include/SFML/System/Vector3.inl:
+
+../SFML-2.1/include/SFML/Window/Context.hpp:
+
+../SFML-2.1/include/SFML/Window/Export.hpp:
+
+../SFML-2.1/include/SFML/Window/GlResource.hpp:
+
+../SFML-2.1/include/SFML/Window/ContextSettings.hpp:
+
+../SFML-2.1/include/SFML/Window/Event.hpp:
+
+../SFML-2.1/include/SFML/Window/Joystick.hpp:
+
+../SFML-2.1/include/SFML/Window/Keyboard.hpp:
+
+../SFML-2.1/include/SFML/Window/Mouse.hpp:
+
+../SFML-2.1/include/SFML/Window/VideoMode.hpp:
+
+../SFML-2.1/include/SFML/Window/Window.hpp:
+
+../SFML-2.1/include/SFML/Window/WindowHandle.hpp:
+
+../SFML-2.1/include/SFML/Window/WindowStyle.hpp:
---
+++ Rush_Hour/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Sietse & Wessel
+ *
+ * Created on 11 March 2014, 11:08
+ */
+
+#include <cstdlib>
+#include <SFML/Window.hpp>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    sf::Window window;
+    return 0;
+}
+
